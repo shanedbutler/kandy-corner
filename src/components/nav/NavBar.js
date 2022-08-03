5685ae883a3cfa97fcdbe4280dcfcbeef244f52c
shanedbutler <shanedbutler@gmail.com>
2022-08-03 13:36:50 -0400

    add location list component

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,6 +6,9 @@ export const NavBar = () => {
 
     return (
         <ul className="navbar">
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/locations">Locations</Link>
+            </li>
             <li className="navbar__item navbar__logout">
                 <Link className="navbar__link" to="" onClick={() => {
                     localStorage.removeItem("kandy_user")
